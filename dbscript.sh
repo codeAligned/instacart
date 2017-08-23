ff23cc945a65bff41423921b3a9b8d65375ca847
Shaun Gosse <responsivedesign@gmail.com>
2017-08-23 11:46:43 -0700

    Now taking in parameters

--- dbscript.sh
+++ dbscript.sh
@@ -1,5 +1,5 @@
 sqlite3 applicants.sqlite3 <<EOS
 .mode csv
-SELECT a, b, c FROM (SELECT COUNT(*) AS a, date(MIN(created_at),'weekday 1') AS b, workflow_state AS c, ROUND((julianday(created_at)-julianday('2015-01-01','weekday 1'))/7) AS d FROM applicants WHERE created_at >= date('2014-01-01') AND created_at <= date('2015-01-01') GROUP BY workflow_state, d) GROUP BY b, c;
+SELECT a, b, c FROM (SELECT COUNT(*) AS a, date(MIN(created_at),'weekday 1') AS b, workflow_state AS c, ROUND((julianday(created_at)-julianday('$1','weekday 1'))/7) AS d FROM applicants WHERE created_at >= date('$1') AND created_at <= date('$2') GROUP BY workflow_state, d) GROUP BY b, c;
 EOS
 
