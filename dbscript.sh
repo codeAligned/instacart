5818743b0867177dcfc384721181c9c75e833091
Shaun Gosse <responsivedesign@gmail.com>
2017-08-23 11:53:57 -0700

    Added header

--- dbscript.sh
+++ dbscript.sh
@@ -1,3 +1,4 @@
+echo "count,week,workflow_state";
 sqlite3 applicants.sqlite3 <<EOS
 .mode csv
 SELECT a, b, c FROM (SELECT COUNT(*) AS a, date(MIN(created_at),'weekday 1') AS b, workflow_state AS c, ROUND((julianday(created_at)-julianday('$1','weekday 1'))/7) AS d FROM applicants WHERE created_at >= date('$1') AND created_at <= date('$2') GROUP BY workflow_state, d) GROUP BY b, c;
